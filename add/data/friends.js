10743b63c85745e29e99f12745c4264ecd7e341f
Alain Joseph <alainrjoseph@gmail.com>
2019-06-06 15:08:57 -0400

    complete

---
+++ add/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [
+	{
+		name: "Zoe Saldana",
+		photo: "https://www.imdb.com/name/nm0757855/mediaviewer/rm2941485568",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Gabrielle Union",
+		photo: "https://www.imdb.com/name/nm0005517/mediaviewer/rm1717143808",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Nia Long",
+		photo: "https://www.imdb.com/name/nm0000505/mediaviewer/rm2235908608",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Yaya Dacosta",
+		photo: "https://www.imdb.com/name/nm1861624/mediaviewer/rm2553650432",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Tika Sumpter",
+		photo: "https://irp-cdn.multiscreensite.com/ce0ec9f3/dms3rep/multi/mobile/TIKA+SUMPTER.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	},
+	{
+		name: "Danai Guirra",
+		photo: "https://www.picclickimg.com/d/l400/pict/283076864685_/Danai-Gurira-SIGNED-4x6-Photograph-Michonne-on-The.jpg",
+		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	},
+	{
+		name: "Dania Ramirez",
+		photo: "https://i.pinimg.com/originals/82/80/e7/8280e76c48532044999d0b0b22d501ee.jpg",
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}
+];
+
+module.exports = friends;
